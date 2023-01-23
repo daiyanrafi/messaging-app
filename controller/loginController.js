4b5494c6e0ddc68d67ea6ac4fcd38904e4ba4954
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-01-23 20:42:32 +0600

    token 23jan 2042

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,8 +1,52 @@
+//external imports
+const bcrypt = require("bcrypt");
+//internal imports
+const User = require("../models/People");
+
 // get login page
 function getLogin(req, res, next) {
   res.render("index");
 }
 
+//do login 
+async function login(req, res, next) {
+  try {
+    //find user email/username
+    const user = await User.findOne({
+      //mongoose or operator
+      $or: [{ email: req.body.username}, { mobile: req.body.username}],
+    });
+
+    if(user && user._id) {
+      //comparing my hashed passowrd with compare bcrypt method
+      const isValidPassowrd = await bcrypt.compare(
+        //this one is plain text
+        req.body.password,
+        //this one what database saved in hashed
+        user.password
+      );
+
+      if(isValidPassowrd) {
+        //prerpare token
+        const userObject = {
+          username: user.name,
+          mobile: user.mobile,
+          email: user.email,
+          role: "user",
+        };
+
+        //generate token 
+        const token = jwt.sign(userObject, )
+      } else {
+      }
+    } else {
+
+    }
+     
+  } catch (err) {
+
+  }
+}
 
 module.exports = {
   getLogin,
